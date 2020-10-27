50de003c270874ee7025992517147aa967a88532
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-27 15:34:40 -0600

    Create test for admin nav bar

---
+++ spec/features/navigation/admin_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'As an admin' do
+  describe 'I see the same links as a regular user' do
+    it 'Plus the following links, a link to my admin dashboard ("/admin"), a link to see all users ("/admin/users")' do
+      jake = User.create!(name: 'JakeBob',
+                          address: '124 Main St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: '80202',
+                          email: 'JBob1234@hotmail.com',
+                          password: 'heftybags',
+                          password_confirmation: 'heftybags',
+                          role: 2)
+                        
+
+      visit '/login'
+      fill_in :email, with: jake.email
+      fill_in :password, with: jake.password
+
+      
+      expect(page).to have_link('Home')
+      expect(page).to have_link('All Items')
+      expect(page).to have_link('All Merchants')
+      expect(page).to have_link('Cart')
+      expect(page).to have_link('Register')
+      expect(page).to have_content('0')
+      click_on "Login"
+      expect(page).to have_link('My Profile')
+      expect(page).to have_link('Admin Dashboard')
+      expect(page).to have_link('All Users')
+      expect(page).to have_link('Log Out')
+      expect(page).not_to have_link('Cart')
+      expect(page).not_to have_link('Log In')
+      expect(page).not_to have_link('Register')
+      expect(page).not_to have_content('0')
+      expect(page).to have_content("Logged in as #{jake.name}")
+    end
+  end
+end
