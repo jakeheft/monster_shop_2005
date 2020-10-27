055b44e7765a5c5e1d82128e094e7aae4096a447
Jake Heft <jakeheft@gmail.com>
2020-10-27 11:31:20 -0600

    Update users table to add role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_233249) do
+ActiveRecord::Schema.define(version: 2020_10_27_172822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_233249) do
     t.string "zip"
     t.string "email"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "item_orders", "items"
