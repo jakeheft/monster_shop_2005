17b6151eba4fb47c8c803d5de0d04c0428d91798
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-27 14:56:39 -0600

    Remove duplicate actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,54 +1,5 @@
 class SessionsController < ApplicationController
   def new
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    
-=======
-
-=======
-
-  end
-
-  def create
-    user = User.find_by(email: params[:username])
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Logged in as #{user.name}!"
-      redirect_to '/'
-    else
-      flash.now[:error] = "Sorry, your credentials are bad."
-      render :new
-    end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    flash.now[:success] = 'You have successfully logged out!'
-    redirect_to '/'
->>>>>>> Stashed changes
-  end
-
-  def create
-    user = User.find_by(email: params[:username])
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Logged in as #{user.name}!"
-      redirect_to '/'
-    else
-      flash.now[:error] = "Sorry, your credentials are bad."
-      render :new
-    end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    flash.now[:success] = 'You have successfully logged out!'
-    redirect_to '/'
->>>>>>> Stashed changes
-  end
-end
-=======
 
   end
 
@@ -65,4 +16,3 @@ end
     end
   end
 end
->>>>>>> f7959e2214cf5aef0394a2a72ed7b1cd3100c001
