bb25ead5b3401df76a25f97bea485651f8742e1e
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-27 16:51:59 -0600

    Update test for merchant auth to admin

--- spec/features/navigation/admin_spec.rb
+++ spec/features/navigation/admin_spec.rb
@@ -14,20 +14,18 @@ describe 'As an admin' do
                           password: 'heftybags',
                           password_confirmation: 'heftybags',
                           role: 2)
-                        
 
       visit '/login'
       fill_in :email, with: jake.email
       fill_in :password, with: jake.password
 
-      
       expect(page).to have_link('Home')
       expect(page).to have_link('All Items')
       expect(page).to have_link('All Merchants')
       expect(page).to have_link('Cart')
       expect(page).to have_link('Register')
       expect(page).to have_content('0')
-      click_on "Login"
+      click_on 'Login'
       expect(page).to have_link('My Profile')
       expect(page).to have_link('Admin Dashboard')
       expect(page).to have_link('All Users')
@@ -37,6 +35,27 @@ describe 'As an admin' do
       expect(page).not_to have_link('Register')
       expect(page).not_to have_content('0')
       expect(page).to have_content("Logged in as #{jake.name}")
+    end
+  end
+  describe 'When I try to access any path that begins with the following, then I see a 404 error:' do
+    it "'/merchant' '/cart'" do
+      jake = User.create!(name: 'JakeBob',
+                          address: '124 Main St',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: '80202',
+                          email: 'JBob1234@hotmail.com',
+                          password: 'heftybags',
+                          password_confirmation: 'heftybags',
+                          role: 2)
+
+      visit "/merchant"
+
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit "/cart"
+
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end
   end
 end
