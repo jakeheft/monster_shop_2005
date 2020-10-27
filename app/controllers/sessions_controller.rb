1c69b4d39a4c09b63a913dc5e6954be494281aa2
Jake Heft <jakeheft@gmail.com>
2020-10-27 13:53:58 -0600

    Add functionality for merchant to log in and be directed to merchant dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     session[:user_id] = user.id
     flash[:success] = "You are now logged in"
-    redirect_to '/profile'
+    if user.role == 3
+      redirect_to "/admin"
+    elsif user.role == 2
+      redirect_to "/merchant"
+    else
+      redirect_to '/profile'
+    end
   end
 end
