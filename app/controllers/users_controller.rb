98b429cb394d137a9cedbb0e2af3d52cff9eee04
John Kim <abcdefghijohnk@gmail.com>
2020-10-26 20:57:03 -0600

    update view to repopulate and output flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @new_user = User.new(user_params)
   end
 
   def create
-    new_user = User.new(user_params)
-    if new_user.save
+    @new_user = User.new(user_params)
+    if @new_user.save
       redirect_to '/profile', notice: "You are now registered and logged in"
+    elsif @new_user.valid_email == false
+      flash[:notice] = "That email address is already in use!"
+      render :new
     else
       redirect_to '/register', notice: "You are missing required field(s)"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
 
   validates :password, confirmation: { case_sensitive: true }
   validates :password_confirmation, presence: true
+
+  def valid_email
+    return false if User.find_by(email: self.email)
+  end
 end
