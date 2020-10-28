51bab64e758cfe3c2684f72c1632ce2991faebea
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-10-28 15:55:43 -0600

    create sad path test for invalid password entries

--- spec/features/users/password_spec.rb
+++ spec/features/users/password_spec.rb
@@ -28,5 +28,27 @@ describe "As a registered user" do
       expect(page).to have_content('Your password has been changed')
       expect(jake.password_digest).not_to eq(password_digest)
     end
+
+    it "I am able to edit my password and am redirected to my profile page" do
+      jake = User.create!( name: 'JakeBob',
+                           address: '124 Main St',
+                           city: 'Denver',
+                           state: 'Colorado',
+                           zip: '80202',
+                           email: 'JBob1234@hotmail.com',
+                           password: 'heftybags',
+                           password_confirmation: 'heftybags',
+                           role: 0
+                          )
+      visit '/login'
+
+      fill_in :email, with: 'JBob1234@hotmail.com'
+      fill_in :password, with: 'heftybags'
+      click_button 'Login'
+      visit '/profile/password'
+      click_button 'Change Password'
+      expect(current_path).to eq('/profile/password')
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
   end
 end
