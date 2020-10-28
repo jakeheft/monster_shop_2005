38f1abf47aab593f75830c0bf6d46b84826fcf90
Jake Heft <jakeheft@gmail.com>
2020-10-27 18:55:40 -0600

    Add test to log out

--- spec/features/sessions/login_spec.rb
+++ spec/features/sessions/login_spec.rb
@@ -197,3 +197,31 @@ describe "As an already registered visitor" do
     end
   end
 end
+### Come back to this to add cleared shopping cart
+describe "As a logged in user, merchant, or admin" do
+  describe "When I visit the logout path, I am redirected to the home page" do
+    it "I see a flash message that I'm logged out" do
+      admin = User.create(
+        name: 'JakeBob',
+        address: '124 Main St',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '80202',
+        email: 'JBob1234@hotmail.com',
+        password: 'heftybags',
+        password_confirmation: 'heftybags',
+        role: 2
+      )
+      visit "/login"
+
+      fill_in :email, with: "JBob1234@hotmail.com"
+      fill_in :password, with: "heftybags"
+      click_button "Login"
+
+      click_link "Logout"
+
+      expect(current_path).to eq("/")
+      expect(page).to eq("You are now logged out")
+    end
+  end
+end
