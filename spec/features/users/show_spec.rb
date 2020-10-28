2f6fd10a5cec135ef66a21379d593a3165ae50f4
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-10-28 13:16:56 -0600

    created test to click edit profile

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -20,11 +20,35 @@ describe "As a registered user" do
       click_button "Login"
 
       visit '/profile'
-save_and_open_page
+
       expect(page).to have_content("Name: #{jake.name}")
       expect(page).to have_content("Address: #{jake.full_address}")
       expect(page).to have_content("Email: #{jake.email}")
       expect(page).to have_link('Edit Profile')
+    end
+
+    it "I see a link to edit my profile data and when I click the link I'm taken to
+    a page to edit my profile" do
+      jake = User.create!( name: 'JakeBob',
+                           address: '124 Main St',
+                           city: 'Denver',
+                           state: 'Colorado',
+                           zip: '80202',
+                           email: 'JBob1234@hotmail.com',
+                           password: 'heftybags',
+                           password_confirmation: 'heftybags',
+                           role: 0
+                          )
+      visit '/login'
+
+      fill_in :email, with: "JBob1234@hotmail.com"
+      fill_in :password, with: "heftybags"
+      click_button "Login"
+
+      visit '/profile'
+
+      click_link('Edit Profile')
+      expect(current_path).to eq('/profile/edit')
     end
   end
 end
