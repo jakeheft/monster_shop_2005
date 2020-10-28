4237470edf9973be12eef81e0049e8c4c8b4e34c
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-10-28 12:07:16 -0600

    create method for full address

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   def valid_email
     return false if User.find_by(email: self.email)
   end
+
+  def full_address
+    "#{address}, #{city}, #{state}, #{zip}"
+  end
 end
