5484b62df56af3032ca0e594b7d637ab04050d6b
Jake Heft <jakeheft@gmail.com>
2020-10-28 11:14:30 -0600

    Update logout test to clear cart

--- spec/features/sessions/logout_spec.rb
+++ spec/features/sessions/logout_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 describe "As a logged in user, merchant, or admin" do
   describe "When I visit the logout path, I am redirected to the home page" do
     it "I see a flash message that I'm logged out" do
-      admin = User.create(
+      user = User.create(
         name: 'JakeBob',
         address: '124 Main St',
         city: 'Denver',
@@ -13,16 +13,24 @@ describe "As a logged in user, merchant, or admin" do
         email: 'JBob1234@hotmail.com',
         password: 'heftybags',
         password_confirmation: 'heftybags',
-        role: 2
+        role: 0
       )
+      dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      item = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       visit "/login"
 
       fill_in :email, with: "JBob1234@hotmail.com"
       fill_in :password, with: "heftybags"
       click_button "Login"
 
+      visit "/items/#{item.id}"
+
+      click_on "Add To Cart"
+      expect(page).to have_content("Cart: 1")
+
       click_link "Log Out"
 
+      expect(page).to have_content("Cart: 0")
       expect(current_path).to eq("/")
       expect(page).to have_content("You have successfully logged out")
     end
