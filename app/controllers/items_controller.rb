e2d4acf8094098c2cf70430d9a7f4b96142c365a
John Kim <abcdefghijohnk@gmail.com>
2020-10-28 19:05:47 -0600

    update items index view to show top and bottom 5 items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,13 @@ class ItemsController<ApplicationController
       @merchant = Merchant.find(params[:merchant_id])
       @items = @merchant.items
     else
-      @items = Item.all
+      @items = Item.find_enabled_items
     end
   end
 
   def show
     @item = Item.find(params[:id])
+
   end
 
   def new
