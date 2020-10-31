6f24573c86b79813a5b22b6f416a3e51ba24c9fa
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-31 14:49:53 -0600

    Update routes and add profile order index page content

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,8 @@ class Order <ApplicationRecord
   def grandtotal
     item_orders.sum('price * quantity')
   end
+
+  def count_items
+    items.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,9 @@ Rails.application.routes.draw do
   get "/profile", to: "users_dashboard#show"
   get "/profile/edit", to: "users_dashboard#edit"
   patch "/profile", to: "users_dashboard#update"
-  get '/profile/orders', to: "profile_orders#show"
+
+  get '/profile/orders', to: "profile_orders#index"
+  get '/profile/orders/:id', to: "profile_orders#show"
 
 
   get "/password/edit", to: "passwords#edit"
