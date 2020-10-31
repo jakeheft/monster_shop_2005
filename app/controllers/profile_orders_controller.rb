34555644d14df4da360f7e344434cf4ff6aa8e44
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-31 16:54:06 -0600

    Create model method for return items and code cancel order action

--- app/controllers/profile_orders_controller.rb
+++ app/controllers/profile_orders_controller.rb
@@ -1,9 +1,18 @@
 class ProfileOrdersController < ApplicationController
+  def index
+
+  end
+
   def show
     @order = current_user.orders.last
   end
 
-  def index
-
+  def cancel
+    order = Order.find(params[:id])
+    order.update(status: 'cancelled')
+    order.return_items
+    order.item_orders.update(status: "unfulfilled")
+    flash[:notice] = "Your order is now cancelled"
+    redirect_to profile_orders_path
   end
 end
