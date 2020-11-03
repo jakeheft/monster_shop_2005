f2db574c28fefd0e0078ede6f27fa9bb22c87600
Jake Heft <jakeheft@gmail.com>
2020-11-03 10:23:06 -0700

    Correct tests that now require merchant ids in item orders

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -101,45 +101,5 @@ describe Merchant, type: :model do
 
       expect(bike_shop.pending_orders).to eq([order_1])
     end
-
-    it '#item_qty' do
-      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
-      print_shop = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80_203)
-      user = User.create!(
-        name: 'JakeBob',
-        address: '124 Main St',
-        city: 'Denver',
-        state: 'Colorado',
-        zip: '80202',
-        email: 'J1234@hotmail.com',
-        password: 'heftybags',
-        password_confirmation: 'heftybags',
-        role: 0
-      )
-      merchant_user = bike_shop.users.create!(name: 'JakeBob',
-                                              address: '124 Main St',
-                                              city: 'Denver',
-                                              state: 'Colorado',
-                                              zip: '80202',
-                                              email: 'Bob1234@hotmail.com',
-                                              password: 'heftybags',
-                                              password_confirmation: 'heftybags',
-                                              role: 1)
-      chain = bike_shop.items.create(name: 'Chain', description: "It'll never break!", price: 50, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
-      tire = bike_shop.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
-      rack = print_shop.items.create(name: 'Bike Rack', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
-      order_1 = Order.create!(name: 'JakeBob', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80_218, user_id: user.id, status: 'Pending')
-      order_item_1 = order_1.item_orders.create!(item: chain, price: chain.price, quantity: 2, status: 'Pending', merchant_id: bike_shop.id)
-      order_item_2 =  order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 'Pending', merchant_id: bike_shop.id)
-      order_item_4 =  order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 'Shipped', merchant_id: bike_shop.id)
-      order_item_3 = order_1.item_orders.create!(item: rack, price: rack.price, quantity: 2, status: 'Pending', merchant_id: print_shop.id)
-
-      order_2 = Order.create!(name: 'JakeBob', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80_218, user_id: user.id, status: 'Pending')
-      order_item_5 = order_2.item_orders.create!(item: chain, price: chain.price, quantity: 2, status: 'Pending', merchant_id: bike_shop.id)
-      order_item_6 =  order_2.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 'Pending', merchant_id: bike_shop.id)
-      order_item_7 =  order_2.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 'Shipped', merchant_id: bike_shop.id)
-
-      expect(bike_shop.item_qty(order_1)).to eq(8)
-    end
   end
 end
