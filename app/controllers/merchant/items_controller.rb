be83374419d985e837a9e3b8ec5dfd54fd075e3e
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-03 14:37:30 -0700

    Create deactivate action for items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,5 +1,14 @@
 class Merchant::ItemsController < Merchant::BaseController
   def index
-    @items = Merchant.find(params["merchant_id"]).items
+    user = User.find(session[:user_id])
+    @items = Merchant.find(user.merchant.id).items
+    @merchant = Merchant.find(user.merchant.id)
+  end
+
+  def deactivate
+    item = Item.find(params[:item_id])
+    item.update(active?: false)
+    redirect_to merchant_items_path
+    flash[:notice] = "This item is no longer for sale"
   end
 end
