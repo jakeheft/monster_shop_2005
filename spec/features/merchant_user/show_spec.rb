ede101c54a01a8a2c97771a33be11a63b4e988fc
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-11-04 14:13:55 -0700

    update test to test for not enough inventory message on order show page

--- spec/features/merchant_user/show_spec.rb
+++ spec/features/merchant_user/show_spec.rb
@@ -223,7 +223,7 @@ describe "As a merchant employee" do
       expect(current_path).to eq("/merchant/orders/#{order_1.id}")
 
       expect(page).to have_link("Fulfill #{chain.name}")
-      expect(page).to_not have_button("Fulfill #{tire.name}")
+      expect(page).to_not have_link("Fulfill #{tire.name}")
       expect(page).to have_content(chain.price)
       expect(page).to have_content(tire.price)
     end
@@ -260,7 +260,7 @@ describe "As a merchant employee" do
       rack = print_shop.items.create(name: 'Bike Rack', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
       order_1 = Order.create!(name: 'JakeBob', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80_218, user_id: user.id, status: 'Pending')
       order_item_1 = order_1.item_orders.create!(item: chain, price: chain.price, quantity: 2, status: 'Pending', merchant_id: bike_shop.id)
-      order_item_2 =  order_1.item_orders.create!(item: tire, price: tire.price, quantity: 15, status: 'Pending', merchant_id: bike_shop.id)
+      order_item_2 =  order_1.item_orders.create!(item: tire, price: tire.price, quantity: 20, status: 'Pending', merchant_id: bike_shop.id)
       order_item_3 = order_1.item_orders.create!(item: rack, price: rack.price, quantity: 2, status: 'Pending', merchant_id: print_shop.id)
 
       visit "/login"
@@ -276,10 +276,11 @@ describe "As a merchant employee" do
 
       expect(page).to_not have_link("Fulfill #{chain.name}")
       expect(page).to_not have_link("Fulfill #{tire.name}")
+      save_and_open_page
       expect(page).to have_content("Chain fulfilled")
       expect(page).to have_content("Chain has been fulfilled")
+      expect(page).to have_content("Can't fullfill - Not enough items in inventory")
       chain.reload
-
       expect(chain.inventory).to eq(3)
 
     end
