2c007ac4dbf285ac9e0ab0bf2d39aec683b2ff78
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-04 13:18:47 -0700

    Convert test to use new form_with field ids

--- spec/features/merchant_user/items/new_spec.rb
+++ spec/features/merchant_user/items/new_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe 'Merchant Items Index Page' do
   describe 'When I visit the merchant items page' do
     before(:each) do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
       @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
       @chain = @meg.items.create(name: 'Chain', description: "It'll never break!", price: 50, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
       @shifter = @meg.items.create(name: 'Shimano Shifters', description: "It'll always shift!", active?: false, price: 180, image: 'https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg', inventory: 2)
@@ -31,15 +31,15 @@ RSpec.describe 'Merchant Items Index Page' do
       click_on 'Add New Item'
 
       expect(page).to have_content('Name')
-      !find_field(:name)
+      !find_field(:item_name)
       expect(page).to have_content('Description')
-      !find_field(:description)
+      !find_field(:item_description)
       expect(page).to have_content('Image')
-      !find_field(:image)
+      !find_field(:item_image)
       expect(page).to have_content('Price')
-      !find_field(:price)
+      !find_field(:item_price)
       expect(page).to have_content('Inventory')
-      !find_field(:inventory)
+      !find_field(:item_inventory)
       expect(page).to have_button('Create Item')
     end
 
@@ -70,11 +70,11 @@ RSpec.describe 'Merchant Items Index Page' do
 
       click_on "Add New Item"
       
-      fill_in :name, with: name
-      fill_in :price, with: price
-      fill_in :description, with: description
-      fill_in :image, with: image_url
-      fill_in :inventory, with: inventory
+      fill_in :item_name, with: name
+      fill_in :item_price, with: price
+      fill_in :item_description, with: description
+      fill_in :item_image, with: image_url
+      fill_in :item_inventory, with: inventory
 
       click_button "Create Item"
 
@@ -108,19 +108,19 @@ RSpec.describe 'Merchant Items Index Page' do
 
       click_on "Add New Item"
       
-      fill_in :name, with: name
-      fill_in :price, with: price
-      fill_in :description, with: description
-      fill_in :image, with: image_url
-      fill_in :inventory, with: inventory
+      fill_in :item_name, with: name
+      fill_in :item_price, with: price
+      fill_in :item_description, with: description
+      fill_in :item_image, with: image_url
+      fill_in :item_inventory, with: inventory
 
       click_button "Create Item"
 
-      expect(find_field(:name).value).to eq(name)
-      expect(find_field(:description).value).to eq(description)
-      expect(find_field(:image).value).to eq(image_url)
-      expect(find_field(:price).value).to eq(price.to_s)
-      expect(find_field(:inventory).value).to eq(inventory)
+      expect(find_field(:item_name).value).to eq(name)
+      expect(find_field(:item_description).value).to eq(description)
+      expect(find_field(:item_image).value).to eq(image_url)
+      expect(find_field(:item_price).value).to eq(price.to_s)
+      expect(find_field(:item_inventory).value).to eq(inventory)
 
       expect(page).to have_content("Inventory can't be blank and Inventory is not a number")
     end
