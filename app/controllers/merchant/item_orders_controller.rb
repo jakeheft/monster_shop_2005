4e85ab3260cdc65a375761c472854028b501f43c
John Kim <abcdefghijohnk@gmail.com>
2020-11-05 16:31:31 -0700

    create spec and itemorder method items_left update controller

--- app/controllers/merchant/item_orders_controller.rb
+++ app/controllers/merchant/item_orders_controller.rb
@@ -1,10 +1,10 @@
 class Merchant::ItemOrdersController < Merchant::BaseController
   def update
-    
+
     item_order = ItemOrder.find(params[:itemorder_id])
     item_order.assign_attributes(status: "Fulfilled")
     if item_order.save
-      item_order.item.update(inventory: (item_order.item.inventory - item_order.quantity))
+      item_order.item.update(inventory: item_order.items_left)
       redirect_to "/merchant/orders/#{item_order.order.id}", notice: "#{item_order.item.name} fulfilled"
     end
   end
