b7eaddb11fc9898577e03feabf6d266511cb1323
Jake Heft <jakeheft@gmail.com>
2020-11-07 12:43:29 -0700

    Add functionality to create new merchant discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,7 +4,19 @@ class Merchant::DiscountsController < Merchant::BaseController
   end
 
   def new
+    # merchant = current_user.merchant
+    # # require "pry"; binding.pry
+    # @discount = merchant.discounts.new
+  end
+
+  def create
     merchant = current_user.merchant
-    @discount = merchant.Discount.new
+    merchant.discounts.create!(discount_params)
+    redirect_to '/merchant/discounts', notice: "Your discount has successfully been created"
+  end
+
+  private
+  def discount_params
+    params.permit(:percent, :min_qty)
   end
 end
