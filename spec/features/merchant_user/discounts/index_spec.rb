d8cb25c8003652d541f7626c2bc0727f79d8a545
Jake Heft <jakeheft@gmail.com>
2020-11-07 14:52:19 -0700

    Add test to see edit discount link on index page

--- spec/features/merchant_user/discounts/index_spec.rb
+++ spec/features/merchant_user/discounts/index_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe "As a merchant employee when I visit the merchant discount index page" do
-  describe "I see a link to create a new discount" do
-    it "When I click the link I'm take to a new discount form page" do
+describe "As a merchant employee" do
+  describe "When I visit the merchant discount index page" do
+    it "I click a link to create a new discount and I'm taken to a new discount form page" do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       merchant_user = bike_shop.users.create!(name: 'JakeBob',
         address: '124 Main St',
@@ -21,6 +21,31 @@ describe "As a merchant employee when I visit the merchant discount index page" 
       click_link "Create A New Bulk Discount"
 
       expect(current_path).to eq('/merchant/discounts/new')
+    end
+
+    it "I click on a link to edit an existing discount I'm taken to an edit form" do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      merchant_user = bike_shop.users.create!(name: 'JakeBob',
+        address: '124 Main St',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '80202',
+        email: 'Bob1234@hotmail.com',
+        password: 'heftybags',
+        password_confirmation: 'heftybags',
+        role: 1
+      )
+      discount = bike_shop.discounts.create(
+        percent: 10,
+        min_qty: 10
+      )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_user)
+
+      visit "/merchant/discounts"
+
+      click_button "Edit Discount"
+
+      expect(current_path).to eq("/merchant/discounts/#{discount.id}/edit")
     end
   end
 end
