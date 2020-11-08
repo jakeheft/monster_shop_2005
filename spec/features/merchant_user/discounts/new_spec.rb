31d18dcdd1f8431eaf8bc2f675868d068918e203
Jake Heft <jakeheft@gmail.com>
2020-11-08 09:28:57 -0700

    Add sad path testing for new discount

--- spec/features/merchant_user/discounts/new_spec.rb
+++ spec/features/merchant_user/discounts/new_spec.rb
@@ -24,12 +24,72 @@ describe "As a merchant employee" do
       click_button "Create Discount"
 
       discount = Discount.last
-      
+
       expect(current_path).to eq('/merchant/discounts')
       within "#discount-#{discount.id}" do
         expect(page).to have_content(discount.min_qty)
         expect(page).to have_content("10%")
       end
+    end
+
+    it "If I enter incorrect attributes, I get an error" do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      merchant_user = bike_shop.users.create!(name: 'JakeBob',
+        address: '124 Main St',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '80202',
+        email: 'Bob1234@hotmail.com',
+        password: 'heftybags',
+        password_confirmation: 'heftybags',
+        role: 1
+      )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_user)
+
+      visit "/merchant/discounts/new"
+
+      fill_in :min_qty, with: 0
+      fill_in :percent, with: 10
+
+      click_button "Create Discount"
+
+      expect(page).to have_content("Must enter a valid percentage and minimum quantity")
+
+      visit "/merchant/discounts/new"
+
+      fill_in :min_qty, with: -5
+      fill_in :percent, with: 10
+
+      click_button "Create Discount"
+
+      expect(page).to have_content("Must enter a valid percentage and minimum quantity")
+
+      visit "/merchant/discounts/new"
+
+      fill_in :min_qty, with: 0.5
+      fill_in :percent, with: 10
+
+      click_button "Create Discount"
+
+      expect(page).to have_content("Must enter a valid percentage and minimum quantity")
+
+      visit "/merchant/discounts/new"
+
+      fill_in :percent, with: 0
+      fill_in :min_qty, with: 5
+
+      click_button "Create Discount"
+
+      expect(page).to have_content("Must enter a valid percentage and minimum quantity")
+
+      visit "/merchant/discounts/new"
+
+      fill_in :percent, with: 100
+      fill_in :min_qty, with: 5
+
+      click_button "Create Discount"
+
+      expect(page).to have_content("Must enter a valid percentage and minimum quantity")
     end
   end
 end
