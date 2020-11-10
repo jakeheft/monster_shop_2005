3076fdb6f1a9de1e56c0a8185b05d17572f3101b
Jake Heft <jakeheft@gmail.com>
2020-11-10 11:06:32 -0700

    Add test for quantity of in cart

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -152,5 +152,29 @@ RSpec.describe Cart do
 
       expect(cart.discounted_price(tire, discount.percent)).to eq(75)
     end
+
+    it '#quantity_of()' do
+      meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      user = User.create!(name: 'JakeBob',
+        address: '124 Main St',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '80202',
+        email: 'Bob1234@hotmail.com',
+        password: 'heftybags',
+        password_confirmation: 'heftybags',
+        role: 0
+      )
+      tire = meg.items.create!(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 1200)
+      discount = meg.discounts.create!(
+        percent: 25,
+        min_qty: 5
+      )
+      cart = Cart.new({
+        tire.id.to_s => 5,
+        })
+      expect { tire.quantity }.to raise_error(NoMethodError)
+      expect(cart.quantity_of(tire)).to eq(5)
+    end
   end
 end
