773918490dbfdd8f369238758ea503ea2d36a343
Jake Heft <jakeheft@gmail.com>
2020-11-10 14:44:15 -0700

    Remove comments

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class OrdersController < ApplicationController
   def new; end
 
@@ -8,14 +6,6 @@ class OrdersController < ApplicationController
     order = Order.create(order_params.merge(user_id: current_user.id).merge(status: 'Pending'))
     if order.save
       order.create_item_orders(cart)
-      # cart.items.each do |item, quantity|
-        # THIS is where I think you'll change each item_order price if it has a discount applied
-        # order.item_orders.create({ item: item,
-        #                            quantity: quantity,
-        #                            price: item.price, #create discount_price method and run a conditional for each item to know whether to apply price or discount_price
-        #                            merchant_id: item.merchant.id,
-        #                            status: "Pending" })
-      # end
       session.delete(:cart)
       redirect_to '/profile/orders', notice: 'Your order has been created'
     else
